6f3124b30ee4894f1fc558eee3acce560721ed09
rnetonet <rneto@rneto.net>
2021-11-11 01:23:34 -0300

    plot results

--- experiments/eyetracking_andersson_dots/plot_rbf_data.py
+++ experiments/eyetracking_andersson_dots/plot_rbf_data.py
@@ -1,10 +1,9 @@
-import argparse
+import glob
 import os
 import os.path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.animation import FuncAnimation
 from matplotlib.lines import Line2D
 from sklearn import metrics
 
@@ -27,109 +26,92 @@ plt.rc("figure", dpi=200)
 plt.rc("figure", figsize=[7, 3.5])
 
 
-#
-# Parse arguments
-#
-parser = argparse.ArgumentParser()
-parser.add_argument("dataset_path")
+def handle(dataset_path, raw=False):
+    dataset_abs_path = os.path.abspath(dataset_path)
+    dataset_filename = os.path.basename(dataset_abs_path)
+    dataset = DataReader(dataset_abs_path)
 
-args = parser.parse_args()
+    #
+    # Plot setup
+    #
+    fig = plt.figure()
 
-dataset_abs_path = os.path.abspath(args.dataset_path)
-dataset_filename = os.path.basename(dataset_abs_path)
+    # Plot data
+    xdata, ydata = [], []
+    fixations_x, fixations_y = [], []
+    fixations_times = []
 
-#
-# Plot setup
-#
-fig = plt.figure()
-fig.gca().set_title(dataset_filename)
+    # Lines/Points
+    (ln,) = plt.plot(xdata, ydata, "-", color=(0, 1, 0), linewidth=0.25)
+    (fixations,) = plt.plot(
+        fixations_x,
+        fixations_y,
+        linestyle="",
+        marker="o",
+        markersize=1,
+        alpha=0.25,
+        color="red",
+    )
 
-# Plot data
-xdata, ydata = [], []
-fixations_x, fixations_y = [], []
-fixations_times = []
+    # Legends
+    custom_legends = [
+        Line2D([0], [0], color=(0, 1, 0), ls="-", linewidth=1),
+        Line2D([0], [0], color="r", ls="-", linewidth=1),
+    ]
+    fig.legend(
+        custom_legends,
+        ["Saccades", "Fixations"],
+        ncol=1,
+        borderaxespad=0,
+        loc="lower center",
+    )
 
-# Lines/Points
-(ln,) = plt.plot(xdata, ydata, "-", color=(0, 1, 0), linewidth=0.25)
-(fixations,) = plt.plot(
-    fixations_x,
-    fixations_y,
-    linestyle="",
-    marker="o",
-    markersize=1,
-    alpha=0.25,
-    color="red",
-)
+    #
+    # Processing dataset
+    #
+    result_rbfchain_predictions = []
+    result_rbfchain_fixations_positions = []
 
-# Legends
-custom_legends = [
-    Line2D([0], [0], color=(0, 1, 0), ls="-", linewidth=1),
-    Line2D([0], [0], color="r", ls="-", linewidth=1),
-]
-fig.legend(
-    custom_legends,
-    ["Saccades", "Fixations"],
-    ncol=1,
-    borderaxespad=0,
-    loc="lower center",
-)
+    if not raw:
+        rbfchain = RBFChain(**{"sigma": 0.01, "lambda_": 0.95, "alpha": 0.01, "delta": 1.0})
 
-#
-# Processing dataset
-#
-dataset = DataReader(dataset_abs_path)
-rbfchain = RBFChain(**{"sigma": 0.01, "lambda_": 0.95, "alpha": 0.01, "delta": 1.0})
+        for index, input_data in enumerate(dataset.distances):
+            probability = rbfchain.add_element(input_data)
 
-result_rbfchain_predictions = []
-result_rbfchain_fixations_positions = []
+            is_fixation = probability >= rbfchain.delta
 
-for index, input_data in enumerate(dataset.distances):
-    probability = rbfchain.add_element(input_data)
-
-    is_fixation = probability >= rbfchain.delta
-
-    if is_fixation:
-        result_rbfchain_predictions.append(1)
-        result_rbfchain_fixations_positions.append(index)
+            if is_fixation:
+                result_rbfchain_predictions.append(1)
+                result_rbfchain_fixations_positions.append(index)
+            else:
+                result_rbfchain_predictions.append(0)
     else:
-        result_rbfchain_predictions.append(0)
+        for index, is_fixation in enumerate(dataset.labels):
+            if is_fixation:
+                result_rbfchain_predictions.append(1)
+                result_rbfchain_fixations_positions.append(index)
+            else:
+                result_rbfchain_predictions.append(0)
 
-#
-# Animation functions
-#
-def init():
+
+    #
+    # Plotting
+    #
     fig.gca().set_xlim((min(dataset.x) - 10, max(dataset.x) + 10))
     fig.gca().set_ylim((min(dataset.y) - 10, max(dataset.y) + 10))
-    return (ln,)
 
+    ln.set_data(dataset.x, dataset.y)
 
-def handle(frame_index):
-    print(frame_index)
-    xdata.append(dataset.x[frame_index])
-    ydata.append(dataset.y[frame_index])
-    ln.set_data(xdata, ydata)
-
-    if result_rbfchain_predictions[frame_index]:
-        fixations_x.append(dataset.x[frame_index])
-        fixations_y.append(dataset.y[frame_index])
+    for fixation_position in result_rbfchain_fixations_positions:
+        fixations_x.append(dataset.x[fixation_position])
+        fixations_y.append(dataset.y[fixation_position])
         fixations.set_data(fixations_x, fixations_y)
 
-    return (ln, fixations)
+    accuracy = metrics.accuracy_score(dataset.labels, result_rbfchain_predictions)
 
+    fig.gca().set_title(f"{dataset_filename=}, {raw=}, {accuracy=:.2f}")
+    plt.savefig(f"results/{dataset_filename}_{raw=}_{accuracy=}.png")
 
-#
-# Start animation
-#
-animation = FuncAnimation(
-    fig,
-    handle,
-    frames=len(dataset.x),
-    init_func=init,
-    interval=1,
-    blit=True,
-    repeat=False,
-)
-
-plt.show()
-
-print(f"{metrics.accuracy_score(dataset.labels, result_rbfchain_predictions):.2f}")
+for dataset_path in glob.glob("data/*.csv"):
+    handle(dataset_path, raw=True)
+    handle(dataset_path, raw=False)
